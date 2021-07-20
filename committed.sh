8ce082a4cbf5ec89de24218441abd8480e4715cb
Brett O'Connor <brettoconnor@dogheadbone.com>
2021-07-20 05:30:10 -0600

    added push to script

--- committed.sh
+++ committed.sh
@@ -3,8 +3,10 @@ NUMBER=$(($RANDOM % 23))
 
 for ((run=1; run <= NUMBER + 1; run++))
 do
-  echo `/usr/games/fortune` > ~/pusher/file.txt
-  /usr/bin/git -C ~/pusher/ add . -A
-  /usr/bin/git -C ~/pusher/ commit -m "`/usr/games/fortune -sn 32`"
+  echo `/usr/games/fortune` > ~/projects/fake_commit/recruiter-test/file.txt
+  /usr/bin/git -C ~/projects/fake_commit/recruiter-test/ add . -A
+  /usr/bin/git -C ~/projects/fake_commit/recruiter-test/ commit -m "`/usr/games/fortune -sn 32`"
 done
 
+cd ~/projects/fake_commit/recruiter-test/
+git push origin master
